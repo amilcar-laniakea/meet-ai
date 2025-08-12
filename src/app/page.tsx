630fb16f03d825db5f8034a67d6088efbb48a4bc
Amilcar Barahona <amilcar.barahona@gmail.com>
2025-08-11 20:00:26 -0400

    Setup Meei Ai Project, including prettier, eslint and Ui libraries

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <div className="text-green-700">Hello!</div>
+      <Button>Hello!</Button>
+    </div>
+  );
+}
